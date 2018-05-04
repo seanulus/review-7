4bdb210a0d8d88347c0b33039e566085e346a129
Sean McDermott <seanjmac@hotmail.com>
2018-05-04 09:07:15 -0700

    Add array of new players in master list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PlayerCreator } from './model/player-model'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  masterPlayerList: PlayerCreator[] = [
+    new PlayerCreator('Sean', 'McDermott', []),
+    new PlayerCreator('Abigail', 'Wallace', []),
+    new PlayerCreator('Walt', 'Whitman', [])
+  ]
 }
