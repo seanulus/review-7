dca83313198de687223d6f182b4038b0099f1429
Sean McDermott <seanjmac@hotmail.com>
2018-05-04 10:26:59 -0700

    Add functionality for edit player component

--- src/app/edit-player/edit-player.component.ts
+++ src/app/edit-player/edit-player.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { PlayerCreator } from '../model/player-model';
 
 @Component({
   selector: 'app-edit-player',
   templateUrl: './edit-player.component.html',
   styleUrls: ['./edit-player.component.css']
 })
-export class EditPlayerComponent implements OnInit {
+export class EditPlayerComponent {
+  @Input() childSelectedPlayer: PlayerCreator;
+  @Output() clickedDone = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit() {
+  finishedEditing() {
+    this.clickedDone.emit();
   }
+  constructor() { }
 
 }
