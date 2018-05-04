012aaad9b4e2bcc04597dade506b300923b5b29c
Sean McDermott <seanjmac@hotmail.com>
2018-05-04 09:41:32 -0700

    Add forms module to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { NewPlayerComponent } from './new-player/new-player.component';
@@ -12,7 +12,8 @@ import { NewPlayerComponent } from './new-player/new-player.component';
     NewPlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
