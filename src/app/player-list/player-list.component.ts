df9aa436bd175b01d76c6793fea48820b773151f
Sean McDermott <seanjmac@hotmail.com>
2018-05-04 10:38:07 -0700

    Add player list component and add functionality to component

---
+++ src/app/player-list/player-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { PlayerCreator } from '../model/player-model';
+
+@Component({
+  selector: 'app-player-list',
+  templateUrl: './player-list.component.html',
+  styleUrls: ['./player-list.component.css']
+})
+export class PlayerListComponent {
+  @Input() childPlayersList: PlayerCreator[];
+  @Output() clickSender = new EventEmitter();
+
+  slectedPlayer = null;
+
+  editButtonClicked(playerToEdit: PlayerCreator) {
+    this.clickSender.emit(playerToEdit);
+  }
+
+  constructor() { }
+
+}
